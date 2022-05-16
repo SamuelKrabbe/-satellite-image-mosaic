d616086d6099f5be6a8df233861fa037b0cbfce4
SamuelKrabbe <samuelkrabbe@hotmail.com>
2022-05-16 16:27:17 -0400

    feito a base da ideia do programa

--- font_code.py
+++ font_code.py
@@ -1,8 +1,21 @@
+import verify_func
+
+
+# lendo as primeiras coordenadas
 xse, yse, xid, yid = map(float, input().split())
 
-xsd = xid
-ysd = yse
-xie = xse
-yie = yid
+# criando um loop para verificar se há interseção entre coordenadas
+while True:
+    try:
+        contador = 1
+        xse2, yse2, xid2, yid2 = map(float, input().split())
 
-coords = {"se": [xse, yse], "sd": [xsd, ysd], "ie": [xie, yie], "id": [xid, yid]}
+        verify_func.verify_se(xse, yse, xid, yid, xse2, yse2)
+        verify_func.verify_id(xse, yse, xid, yid, xid2, yid2)
+
+        contador += 1
+
+    except:
+        break
+
+print(contador)
---
+++ verify_func.py
@@ -0,0 +1,44 @@
+# verificação ponto superior esquerdo
+def verify_se(xse, yse, xid, yid, xse2, yse2):
+    if xse <= xse2 and xse2 <= xid:
+        if yse >= yse2 and yse2 <= yid:
+            xse = xse
+            yse = yse
+        elif yse2 > yse:
+            xse = xse
+            yse = yse2
+
+    elif xse2 < xse:
+        if yse >= yse2 and yse2 <= yid:
+            xse = xse2
+            yse = yse
+        elif yse2 > yse:
+            xse = xse2
+            yse = yse2
+
+    return xse, yse
+
+
+# --------------------------------------------------------------------------------------
+
+# verificação ponto inferior direito
+def verify_id(xse, yse, xid, yid, xid2, yid2):
+    if xse <= xid2 and xid2 <= xid:
+        if yse >= yid2 and yid2 <= yid:
+            xid = xid
+            yid = yid
+
+        elif yid2 < yid:
+            xid = xid
+            yid = yid2
+
+    elif xid2 > xid:
+        if yse >= yid2 and yid2 <= yid:
+            xid = xid2
+            yid = yid
+
+        elif yid2 < yid:
+            xid = xid2
+            yid = yid2
+
+    return xid, yid
