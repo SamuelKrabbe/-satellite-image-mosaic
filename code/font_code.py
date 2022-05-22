66feef263f008116cfac05bd036958fe0bfc8ee7
SamuelKrabbe <samuelkrabbe@hotmail.com>
2022-05-22 15:03:16 -0400

    changes

--- code/font_code.py
+++ code/font_code.py
@@ -1,5 +1,4 @@
-import verify_func
-
+import verify
 
 # lendo as primeiras coordenadas
 xse, yse, xid, yid = map(int, input().split())
@@ -12,19 +11,18 @@ while True:
     try:
         xse2, yse2, xid2, yid2 = map(int, input().split())
 
-        ponto_se = verify_func.verify_se(xse, yse, xid, yid, xse2, yse2)
-        ponto_id = verify_func.verify_id(xse, yse, xid, yid, xid2, yid2)
+        retang = verify.verify_func(xse, yse, xid, yid, xse2, yse2)
 
-        if ponto_se == 0:
+        if retang == 0:
             ret_ad.append([(xse2, yse2), (xid2, yid2)])
             contador += 1
 
     except:
         break
 
-ret_ad.insert(0, [ponto_se, ponto_id])
+ret_ad.insert(0, [retang])
 
 print(contador)
 
-for retangulos in ret_ad:
-    print(", ".join(map(str, retangulos)))
+for retangs in ret_ad:
+    print(", ".join(map(str, retangs)))
