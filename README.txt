be1548ba442abefefd2428e289af61c88b3e8d6e
SamuelKrabbe <samuelkrabbe@hotmail.com>
2022-06-04 21:15:19 -0400

    finalizado

--- README.txt
+++ README.txt
@@ -1,2 +1,4 @@
 # -satellite-image-mosaic
 Projeto de algoritmos e programação I do curso de Ciência da Computação da UFMS
+
+Primeiramente, antes de rodar o programa, deixe as coordenadas que serão usadas já preparedas para serem coladas na linha de comando, a seguir, abra o programa em uma IDE e rode ele, depois disso, cole as coordenadas copiadas na linha de comando e o resultado sera impresso.
--- code/main.py
+++ code/main.py
@@ -5,14 +5,15 @@ Module Docstring
 __author__ = "Samuel Krabbe, Matheus Mota"
 __version__ = "Python 3.10.4"
 
+print("Digite as coordenadas das imagens aqui: ", end="")
+
 # Lendo o primeiro input para usar de base para as próximas comparações
-x_se, y_se, x_id, y_id = map(int, input("Digite a coordenada da imagen: ").split())
+x_se, y_se, x_id, y_id = map(int, input().split())
 
 # Criando um loop para ler as próximas coordenadas e definir o retângulo mínimo
 while True:
     try:
-        x1, y1, x2, y2 = map(int, input("Digite a coordenada da imagen: ").split())
-
+        x1, y1, x2, y2 = map(int, input().split())
         if x1 < x_se:
             x_se = x1
         if y1 > y_se:
@@ -25,6 +26,11 @@ while True:
         break
 
 # imprimindo "1", pois só vai ter um retângulo no final
-print("1")
+print("Quantidade de retângulos mínimos: 1")
 # imprimindo as coordenadas do retângulo mínimo
-print("({}, {}), ({}, {})".format(x_se, y_se, x_id, y_id))
+print(
+    "Coordenadas do retângulo mínimo: ({}, {}), ({}, {})".format(
+        x_se, y_se, x_id, y_id
+    ),
+    end="",
+)
